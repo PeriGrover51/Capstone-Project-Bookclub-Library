1e066d113c0203ee2d55cb45b875cdc9980bed57
Peri Grover <peri.grover@gmail.com>
2026-08-26 14:03:27 -0600

    book service validate(), update() + tests

--- bookclub-server/src/main/java/learn/bookclub/services/BookService.java
+++ bookclub-server/src/main/java/learn/bookclub/services/BookService.java
@@ -32,4 +32,77 @@ public class BookService {
 
         return result;
     }
+
+    public Result<Book> update(Book book) {
+        //update validation: id must be set
+        //id must exist in db
+        Result<Book> result = new Result<>();
+        validate(book, result);
+
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (book.getBookId() <= 0) {
+            result.addErrorMessage("bookId is required.", ResultType.INVALID);
+            return result;
+        }
+
+        Book existing = repository.findById(book.getBookId());
+        if (existing == null) {
+            result.addErrorMessage("book not in db", ResultType.NOT_FOUND);
+        }
+
+        if (result.isSuccess()) {
+            if (repository.update(book)) {
+                result.setpayload(book);
+            } else {
+                result.addErrorMessage("book not found", ResultType.NOT_FOUND);
+            }
+        }
+
+        return result;
+    }
+
+    public Result<Book> create(Book book) {
+        //create validation: id must be 0
+        return null;
+    }
+
+    private void validate(Book book, Result<Book> result) {
+        //validation for create + update:book/ title / author / genre / whenRead cannot be null or blank
+        //whenRead can be in the future, e.g. the book wins the nominations and is set to begin reading next week
+        //link and img_link can be null
+        if (book == null) {
+            result.addErrorMessage("book cannot be null", ResultType.INVALID);
+            return;
+        }
+        if (book.getTitle() == null || book.getTitle().isBlank()) {
+            result.addErrorMessage("title cannot be empty", ResultType.INVALID);
+        }
+        if (book.getAuthor() == null || book.getAuthor().isBlank()) {
+            result.addErrorMessage("author cannot be empty", ResultType.INVALID);
+        }
+        if (book.getGenre() == null || book.getGenre().isBlank()) {
+            result.addErrorMessage("genre cannot be empty", ResultType.INVALID);
+        }
+        if (book.getWhenRead() == null) {
+            result.addErrorMessage("whenRead cannot be empty", ResultType.INVALID);
+        }
+        //(title + author + genre + whenRead) must be unique combination and bookIds don't match (for update purposes)
+
+        if (result.isSuccess()) {
+            List<Book> existingBooks = repository.findAll();
+
+            for (Book existingBook : existingBooks) {
+                if (existingBook.getBookId() != book.getBookId() &&
+                        existingBook.getTitle().equalsIgnoreCase(book.getTitle()) &&
+                        existingBook.getAuthor().equalsIgnoreCase(book.getAuthor()) &&
+                        existingBook.getGenre().equalsIgnoreCase(book.getGenre()) &&
+                        existingBook.getWhenRead().isEqual(book.getWhenRead())) {
+                    result.addErrorMessage("book cannot be duplicate", ResultType.INVALID);
+                }
+            }
+        }
+    }
 }
--- bookclub-server/src/test/java/learn/bookclub/services/BookServiceTest.java
+++ bookclub-server/src/test/java/learn/bookclub/services/BookServiceTest.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class BookServiceTest {
@@ -41,5 +43,98 @@ class BookServiceTest {
 
         assertEquals(expected, actual);
     }
+
+
+    //update tests:
+    //happy, blank / null fields, unset id, invalid id, dupe test
+    @Test
+    void shouldNotUpdateNullFields() {
+        when(repository.findAll()).thenReturn(List.of(TestDataHelper.existingBook()));
+
+        assertTrue(service.update(null).getErrorMessages().contains("book cannot be null"));
+
+        Book nullFields = new Book(1, null, null, null, null, null, null);
+        Result<Book> result = service.update(nullFields);
+
+        assertTrue(result.getErrorMessages().size() == 4);
+
+        verify(repository, never()).update(nullFields);
+    }
+
+    @Test
+    void shouldNotUpdateBlankFields() {
+        when(repository.findAll()).thenReturn(List.of(TestDataHelper.existingBook()));
+
+        Book blankFields = new Book(1, "", "", "", null, null, null);
+        Result<Book> result = service.update(blankFields);
+
+        assertTrue(result.getErrorMessages().size() == 4);
+
+        verify(repository, never()).update(blankFields);
+    }
+
+    @Test
+    void shouldNotUpdateUnsetId() {
+        when(repository.findAll()).thenReturn(List.of(TestDataHelper.existingBook()));
+
+        Book unsetId = TestDataHelper.bookToUpdate();
+        unsetId.setBookId(0);
+        Result<Book> result = service.update(unsetId);
+
+        assertFalse(result.isSuccess());
+        assertTrue(result.getErrorMessages().contains("bookId is required."));
+        assertTrue(result.getErrorMessages().size() == 1);
+
+        verify(repository, never()).update(unsetId);
+    }
+
+    @Test
+    void shouldNotUpdateInvalidId() {
+        when(repository.findAll()).thenReturn(List.of(TestDataHelper.existingBook()));
+        when(repository.findById(999)).thenReturn(null);
+
+        Book badId = TestDataHelper.bookToUpdate();
+        badId.setBookId(999);
+        Result<Book> result = service.update(badId);
+
+        assertFalse(result.isSuccess());
+        assertTrue(result.getErrorMessages().contains("book not in db"));
+        assertTrue(result.getErrorMessages().size() == 1);
+
+        verify(repository, never()).update(badId);
+    }
+
+    @Test
+    void shouldNotUpdateDuplicate() {
+        when(repository.findAll()).thenReturn(List.of(TestDataHelper.existingBook()));
+
+        Book duplicate = TestDataHelper.existingBook();
+        duplicate.setBookId(2);
+        Result<Book> result = service.update(duplicate);
+        //although this id doesnt exist in the db, the validation should add dupes err + return result before calling findById
+
+        assertFalse(result.isSuccess());
+        assertTrue(result.getErrorMessages().contains("book cannot be duplicate"));
+
+        verify(repository, never()).update(duplicate);
+    }
+
+    @Test
+    void updateHappy() {
+        when(repository.findAll()).thenReturn(List.of(TestDataHelper.existingBook()));
+        when(repository.findById(1)).thenReturn(TestDataHelper.existingBook());
+        when(repository.update(TestDataHelper.bookToUpdate())).thenReturn(true);
+
+        Book toUpdate = TestDataHelper.bookToUpdate();
+        Result<Book> result = service.update(toUpdate);
+
+        assertTrue(result.isSuccess());
+        assertEquals(TestDataHelper.bookToUpdate(), result.getpayload());
+
+        verify(repository, times(1)).update(toUpdate);
+    }
+
+    //create tests:
+    //happy, blank / null fields, set id, dupe test
 
 }
