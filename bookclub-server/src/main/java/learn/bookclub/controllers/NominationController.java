34ffe181f99fb8ebfc02b4fe3e28f738eb0b38e8
Peri Grover <peri.grover@gmail.com>
2026-08-27 13:59:40 -0600

    nom controller + postman tests

---
+++ bookclub-server/src/main/java/learn/bookclub/controllers/NominationController.java
@@ -0,0 +1,34 @@
+package learn.bookclub.controllers;
+
+import learn.bookclub.models.Nomination;
+import learn.bookclub.services.NominationService;
+import learn.bookclub.services.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/nominations")
+@CrossOrigin
+public class NominationController {
+
+    @Autowired
+    NominationService service;
+
+    @GetMapping
+    public List<Nomination> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable("id") int id) {
+        Result<Nomination> result = service.findById(id);
+        if(!result.isSuccess()) {
+            return ErrorResponse.build(result);
+        }
+        return new ResponseEntity<>(result.getpayload(), HttpStatus.OK);
+    }
+}
