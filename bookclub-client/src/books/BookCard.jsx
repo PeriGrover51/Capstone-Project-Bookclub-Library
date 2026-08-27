597e54d27792a64c096ce26faa3a5c6f8696b59e
Peri Grover <peri.grover@gmail.com>
2026-08-27 09:41:28 -0600

    added Bookform with create / update functionality, some tailwind styling, updated nav + button links

--- bookclub-client/src/books/BookCard.jsx
+++ bookclub-client/src/books/BookCard.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom"
 
 export default function BookCard({ book }) {
     const { user } = useAuth()
+
+    //add 'edit book' functionality
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src={book.imgLink} alt={book.title} />
+        <div className="w-96 flex-none rounded overflow-hidden shadow-lg">
+            <img className="w-full h-72 object-cover" src={book.imgLink} alt={book.title} />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2 text-center">{book.title}</div>
                 <div className="font-bold text-l mb-2 text-center">{book.author}</div>
@@ -14,7 +16,7 @@ export default function BookCard({ book }) {
                 <a href={book.link} className="font-medium text-blue-600 hover:underline flex justify-center">GoodReads Link</a>
                 {user && 
                 <div className="px-6 pt-4 pb-2 flex justify-center">
-                <Link to="/" 
+                <Link to={`/books/edit/${book.bookId}`} 
                 className="text-black bg-yellow-500 hover:bg-yellow-400 py-2 px-4 mx-2 rounded">
                     Edit
                 </Link>
