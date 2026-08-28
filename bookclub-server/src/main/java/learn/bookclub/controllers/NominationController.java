ee3de911410f150a5457c8cd9cfe8455607eaafb
Peri Grover <peri.grover@gmail.com>
2026-08-28 09:40:47 -0600

    nom controller post mapping + tests

--- bookclub-server/src/main/java/learn/bookclub/controllers/NominationController.java
+++ bookclub-server/src/main/java/learn/bookclub/controllers/NominationController.java
@@ -6,6 +6,7 @@ import learn.bookclub.services.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,6 +33,25 @@ public class NominationController {
         return new ResponseEntity<>(result.getpayload(), HttpStatus.OK);
     }
 
+    //create + edit: check that auth username == nomination.user.username
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Nomination nomination, Authentication authentication) {
+        String requestingUsername = authentication.getName(); //comes from verified JWT, not the request body
+        System.out.println(requestingUsername);
 
+        //this should check that the username from the jwt token and the username in the nom body match.
+        if (!nomination.getUser().getUsername().equals(requestingUsername)) {
+            return new ResponseEntity<>("Logged-in user and nomination body user mismatch", HttpStatus.FORBIDDEN);
+        }
+
+        Result<Nomination> result = service.create(nomination);
+
+        if (!result.isSuccess()) {
+            return ErrorResponse.build(result);
+        }
+        return new ResponseEntity<>(result.getpayload(), HttpStatus.CREATED);
+    }
+
+    //post / create mapping: pull the Authentication obj, get the username from that, and compare to the userid in the request body
     //Put / edit mapping: pull the Authentication obj, get the username from that, and compare to the userid in the request body
 }
