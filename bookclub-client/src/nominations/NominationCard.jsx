67b2881c2f1aae9434367030f402631de23004d6
Peri Grover <peri.grover@gmail.com>
2026-08-31 10:46:50 -0600

    added voting function to NominationCard

--- bookclub-client/src/nominations/NominationCard.jsx
+++ bookclub-client/src/nominations/NominationCard.jsx
@@ -3,11 +3,44 @@ import { useAuth } from '../AuthContext';
 import { Link } from "react-router-dom"
 
 export default function NominationCard({ nomination }) {
-
+    const { token } = useAuth()
     const { user } = useAuth()
 
+    //for holding the user's voting scores
+    const [myScore, setMyScore] = useState(null)
+
+    useEffect(() => {
+        async function fetchMyVote() {
+            const response = await fetch(`http://localhost:8080/api/votes/nomination/${nomination.nominationId}/mine`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            });
+            if (response.status === 204) { //server processed request correctly but user hasn't voted yet
+                setMyScore(null)
+            } else if (response.status == 200) { //server gives the vote
+                const data = await response.json()
+                setMyScore(data.score)
+            }
+        }
+        fetchMyVote()
+    }, [nomination.nominationId, token])
+
+    async function castVote(score) {
+        const response = await fetch(`http://localhost:8080/api/votes/nomination/${nomination.nominationId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ score }),
+        });
+        const data = await response.json()
+        setMyScore(data.score) //update local score to reflect new vote immediately
+    }
+
     return (
-        <div className="w-150 lg:flex flex-none">
+        <div className="w-full lg:flex flex-none">
             <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                 <div className="mb-6">
                     <div className="text-gray-900 font-bold text-xl mb-2">{nomination.title} by {nomination.author}</div>
@@ -15,7 +48,7 @@ export default function NominationCard({ nomination }) {
                     <p className="text-gray-700 text-base">Nominated By {nomination.user.username}</p>
                 </div>
                 {user && user.username === nomination.user.username &&
-                <div className="px-1 pt-4 pb-2 flex">
+                <div className="px-1 pt-4 pb-2 flex justify-center items-center">
                     <Link to={`/nominations/edit/${nomination.nominationId}`} 
                     className="text-black bg-yellow-500 hover:bg-yellow-400 py-2 px-4 mx-2 rounded">
                         Edit
@@ -26,6 +59,22 @@ export default function NominationCard({ nomination }) {
                     </Link>
                 </div>
                 }
+                <div className="border border-black rounded bg-gray-50">
+                    <div className="w-full text-center pt-4">
+                        <h4 className="">Vote:</h4>
+                    </div>
+                    <div className="w-full text-center">
+                        {[1, 2, 3, 4, 5].map(score => (
+                        <button 
+                            key={score}
+                            onClick={() => castVote(score)}
+                            className={myScore === score ? 'text-black bg-blue-500 hover:bg-blue-400 px-6 py-3 mt-4 rounded font-semibold text-center' : 'text-black bg-white-500 hover:bg-gray-400 px-6 py-3 mt-4 rounded font-semibold text-center'}
+                        >
+                            {score}
+                        </button>
+                        ))}
+                    </div>
+                </div>
             </div>
 
         </div>
