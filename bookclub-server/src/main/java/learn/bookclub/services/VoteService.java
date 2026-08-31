13f2e0555674a2da842c672cf3fb69a3d66cbb20
Peri Grover <peri.grover@gmail.com>
2026-08-31 09:19:22 -0600

    voteservice saveVote() + tests

--- bookclub-server/src/main/java/learn/bookclub/services/VoteService.java
+++ bookclub-server/src/main/java/learn/bookclub/services/VoteService.java
@@ -1,6 +1,10 @@
 package learn.bookclub.services;
 
+import learn.bookclub.models.Nomination;
+import learn.bookclub.models.User;
 import learn.bookclub.models.Vote;
+import learn.bookclub.repos.NominationRepository;
+import learn.bookclub.repos.UserRepository;
 import learn.bookclub.repos.VoteRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +14,14 @@ import java.util.List;
 public class VoteService {
 
     private VoteRepository repository;
-    //may need to add userrepo and nomrepo here for saveVote method
+    private UserRepository userRepository;
+    private NominationRepository nominationRepository;
 
-    public VoteService(VoteRepository repository) {
+
+    public VoteService(VoteRepository repository, UserRepository userRepository, NominationRepository nominationRepository) {
         this.repository = repository;
+        this.userRepository = userRepository;
+        this.nominationRepository = nominationRepository;
     }
 
     public List<Vote> findByNominationId(int nominationId) {
@@ -31,10 +39,39 @@ public class VoteService {
 
     public Result<Vote> saveVote(String username, int nominationId, int score) {
         //current thought: frontend / controller doesn't send us a constructed vote - we do that here, then save to the repo / db
+
         //validation: score is between 1-5, user and nomination exist in db
+        Result<Vote> result = new Result<>();
+
+        if (score < 1 || score > 5) {
+            result.addErrorMessage("score must be between 1 and 5", ResultType.INVALID);
+        }
+
+        User user = userRepository.findByUsername(username);
+        Nomination nomination = nominationRepository.findById(nominationId);
+        if (user == null) {
+            result.addErrorMessage("user not in db", ResultType.INVALID);
+        }
+        if (nomination == null) {
+            result.addErrorMessage("nomination not in db", ResultType.NOT_FOUND);
+        }
+        if (!result.isSuccess()) {
+            return result;
+        }
+
         //check if vote already exists in repo:
-            //if yes, then fetch that vote, set the new score, and send to repo.save
-            //if no, then create new vote with a null id, set userid + nomid, set score, and send to repo.save
-        return null;
+        Vote exists = repository.findByUserIdAndNominationId(user.getUserId(), nominationId);
+
+        //vote does not already exist (new vote) == create new vote id with id 0 and save
+        if (exists == null) {
+            Vote newVote = new Vote(0, user.getUserId(), nominationId, score);
+            result.setpayload(repository.saveVote(newVote));
+        } else {
+            //vote DOES exist (update vote) = set new score and save
+            exists.setScore(score);
+            result.setpayload(repository.saveVote(exists));
+        }
+
+        return result;
     }
 }
