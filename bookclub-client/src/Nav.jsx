5563a7060e0a5d4c8845452c92795fe0a300ae05
Peri Grover <peri.grover@gmail.com>
2026-09-02 15:13:46 -0600

    navitem hover bg

--- bookclub-client/src/Nav.jsx
+++ bookclub-client/src/Nav.jsx
@@ -45,7 +45,7 @@ export default function Nav() {
                             </p>
                             <button
                                 onClick={logout}
-                                className="mt-2 w-full rounded-md px-3 py-2 text-left text-m text-slate-100 hover:bg-[#663300] hover:text-white"
+                                className="mt-2 w-full rounded-md px-3 py-2 text-left text-m text-slate-100 hover:bg-[#cdb488] hover:text-white"
                             >
                                 Log out
                             </button>
