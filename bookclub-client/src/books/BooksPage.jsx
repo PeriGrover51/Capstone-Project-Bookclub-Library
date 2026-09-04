0cb87fec002cf1d3c1662b28febeec0fa5b16103
Peri Grover <peri.grover@gmail.com>
2026-09-04 13:26:51 -0600

    books search includes author + genre

--- bookclub-client/src/books/BooksPage.jsx
+++ bookclub-client/src/books/BooksPage.jsx
@@ -52,7 +52,11 @@ export default function BooksPage() {
     //search bar functionality
     const [searchQuery, setSearchQuery] = useState('')
 
-    const filteredBooks = books.filter(book => book.title.toLowerCase().includes(searchQuery.toLowerCase()))
+    const filteredBooks = books.filter(book => 
+        book.title.toLowerCase().includes(searchQuery.toLowerCase())
+        || book.author.toLowerCase().includes(searchQuery.toLowerCase())
+        || book.genre.toLowerCase().includes(searchQuery.toLowerCase())
+    )
 
 
     //pagination functionality
@@ -83,7 +87,7 @@ export default function BooksPage() {
             <h1 className="font-bold text-4xl pl-6 taped-note--header taped-note">LIBRARY</h1>
             <div className="ml-6 library-card">
                 <input className=" ml-6"
-                type="text" placeholder="search by title..." value={searchQuery} 
+                type="text" placeholder="search by keyword..." value={searchQuery} 
                 onChange={(e) => {
                     setSearchQuery(e.target.value);
                     //add page-reset here
